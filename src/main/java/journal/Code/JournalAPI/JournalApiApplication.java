0fda53b06c23ea95cda954eaed54be41b23a12a2
PratyushMakkar <85073808+PratyushMakkar@users.noreply.github.com>
2021-11-21 23:54:32 -0400

    Update JournalApiApplication.java
    (proxyBeanMethods = false)

--- src/main/java/journal/Code/JournalAPI/JournalApiApplication.java
+++ src/main/java/journal/Code/JournalAPI/JournalApiApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class},
+		      proxyBeanMethods = false)
 public class JournalApiApplication {
 
 	public static void main(String[] args) {
