b66c3f897124181faa2bca887c76524d4dab16a2
PratyushMakkar <85073808+PratyushMakkar@users.noreply.github.com>
2021-11-21 23:40:58 -0400

    Update MainConfiguration.java
    Turned to false

--- src/main/java/journal/Code/JournalAPI/ConfigurationClasses/MainConfiguration.java
+++ src/main/java/journal/Code/JournalAPI/ConfigurationClasses/MainConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration(proxyBeanMethods=true)
+@Configuration(proxyBeanMethods=false)
 @Profile({"production"})
 public class MainConfiguration {
 
